9958c70ada9375e80f9c565e393f3605c0d3b66a
avitoras <yamadeypon@gmail.com>
2024-10-12 19:27:53 +0300

    added OS and kernel information

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fetch/gofetch"
-	// "fmt"
+	"fmt"
 	"os"
 	"os/user"
+	"runtime"
+	// "strings"
+	"github.com/shirou/gopsutil/v4/host"
 )
 
 func main() {
 	// user@hostname
-	user, _ := user.Current()
-	host, _ := os.Hostname()
-	userhost := user.Username + "@" + host
+	username, _ := user.Current()
+	hostname, _ := os.Hostname()
+	userhost := username.Username + "@" + hostname
 	gofetch.PrintWithUnderline(userhost)
 
 	// OS
-	///fmt.Println("OS: ",os.ReadFile())
+	dist, _, version, _ := host.PlatformInformation()
+	fmt.Println("OS:", dist, version)
+
+	// Kernel
+	// kernel, _ := runtime.GOOS
+	kernelver, _ := host.KernelVersion()
+	fmt.Println("Kernel:", runtime.GOOS,kernelver)
 }
